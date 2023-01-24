f9622dbf3494c3bdbe38df3c645eab482d6ddc67
Mikhail Kondrashin <misha2kon@mail.ru>
2023-01-25 01:15:33 +0300

    try to fix admin css problem

--- django_ideas_site/settings.py
+++ django_ideas_site/settings.py
@@ -118,7 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -130,8 +131,8 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'ideas-home'
 LOGIN_URL = 'login'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 CSRF_TRUSTED_ORIGINS = ['https://perveevm.ru']
